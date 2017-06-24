db8cdf4ed2b75a6628b782f7118f257cf5fcb231
melvin olive <melvinolive2@gmail.com>
2017-06-24 16:27:36 +0000

    functionality of ruby to html

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,8 @@
 class TodoController < ApplicationController
     def index 
     end
-    def show
+    def show 
+        @todo_description= "Finish Geometry"
+        @todo_pomodoro_estimate= 3
     end
 end
