f6770d01fe96ae0c578f6ef8f6ce946d86b32b2d
melvin olive <melvinolive2@gmail.com>
2017-06-25 16:24:41 +0000

    save and redirect create action

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,4 +5,15 @@ class TodoController < ApplicationController
     def show
       @todo = Todo.find_by_id(params[:id])
     end
+    
+    def new #just where our form lives
+    end
+    
+    def create 
+        t= Todo.new
+        t.description= params["description"]
+        t.pomodoro_estimate= params["pomodoro_estimate"]
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
+  get 'todo/new', to: 'todo#new'
   get 'todo/show/:id', to: 'todo#show'
+  get 'todo/create', to: 'todo#create'
 end
+
